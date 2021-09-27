6ebeb80bbc8eeef2994ff4dd4fff50578ec6da3e
SahilShrivastava-Dev <sahilshrivastava28@gmail.com>
2021-09-27 22:15:41 +0530

    New Commit

--- Laila.py
+++ Laila.py
@@ -1,14 +1,14 @@
 import pyttsx3  #pip install pyttsx3
-import datetime  #module
+import datetime  
 import speech_recognition as sr
 import wikipedia
 import smtplib
 import webbrowser as wb
-import os  #inbuilt
+import os  
 import pyautogui
 import psutil  #pip install psutil
 import pyjokes  # pip install pyjokes
-import requests, json  #inbuilt
+import requests, json  
 
 engine = pyttsx3.init()
 engine.setProperty('rate', 190)
